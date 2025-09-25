bc3498d263da0b7e26e42dcb52e62ad98ded23f3
OllyDI <choiseungho11@hanmail.net>
2025-09-25 13:18:23 +0900

    25.09.25

---
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -0,0 +1,18 @@
+package hello.exception.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * ResponseStatusExceptionResolver
+ * 예외에 따라서 HTTP 상태 코드 지정 두 가지 경우 처리
+ * @ResponseStatus
+ * ResponseStatusException
+ * reason을 MessageSource 에서 찾는 기능 제공 -> messages.properties
+ */
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
+public class BadRequestException extends RuntimeException {
+
+
+}
